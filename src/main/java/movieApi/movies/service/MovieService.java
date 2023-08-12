34869c35d830313c685eab3e73a9d13d4273ca0f
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-08-12 11:38:05 -0500

    re-worked find/upload methods
    findMovieByImdbId: streamed for a more readble and compact code
    
    uploadNewMovie: added ID checker to make sure there were no
    duplicate imdbIds.

--- src/main/java/movieApi/movies/service/MovieService.java
+++ src/main/java/movieApi/movies/service/MovieService.java
@@ -1,17 +1,17 @@
 package movieApi.movies.service;
 
 import movieApi.movies.converter.Converter;
-import movieApi.movies.dto.CreateMovieRequest;
-import movieApi.movies.dto.MovieDTO;
+import movieApi.movies.dto.request.CreateMovieRequest;
+import movieApi.movies.dto.response.MovieDTO;
 import movieApi.movies.entity.Movie;
-import movieApi.movies.exception.MovieNotFoundException;
 import movieApi.movies.repository.MovieRepository;
+import movieApi.movies.utils.CustomIdMaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,15 +24,25 @@ public class MovieService {
                 .map(Converter::MovieToDTO)
                 .collect(Collectors.toList());
     }
-    public MovieDTO findMovieByImdbId(String imdbId) {
-        Movie movie = repository.findMovieByImdbId(imdbId)
-                .orElseThrow(MovieNotFoundException::new);
-        return Converter.MovieToDTO(movie);
+    public Optional<MovieDTO> findMovieByImdbId(String imdbId) {
+        return repository.findMovieByImdbId(imdbId)
+                .map(Converter::MovieToDTO);
     }
 
     public MovieDTO uploadNewMovie(CreateMovieRequest request) {
+        String imdbId = CustomIdMaker.generateRandomNumberIdentifier();
+        boolean isAvailable = false;
+        // Find better way to determine weather or not the id is already in use
+        while (!isAvailable) {
+            if (findMovieByImdbId(imdbId).isEmpty()) {
+                isAvailable = true;
+            } else {
+                imdbId = CustomIdMaker.generateRandomNumberIdentifier();
+            }
+        }
+
         Movie movie = repository.insert(new Movie(
-                String.format("tt%s", generateRandomNumberIdentifier()),
+                imdbId,
                 request.title(),
                 request.releaseDate(),
                 request.trailerLink(),
@@ -43,22 +53,5 @@ public class MovieService {
         ));
 
         return Converter.MovieToDTO(movie);
-    }
-
-    // Define the pool of characters for generating numbers in the identifier
-    private static final String NUMBER_POOL = "0123456789";
-
-    // Method to generate a random number identifier
-    private static String generateRandomNumberIdentifier() {
-        Random random = new Random();
-        StringBuilder stringBuilder = new StringBuilder(7);
-
-        for (int i = 0; i < 7; i++) {
-            int randomIndex = random.nextInt(NUMBER_POOL.length());
-            char randomChar = NUMBER_POOL.charAt(randomIndex);
-            stringBuilder.append(randomChar);
-        }
-
-        return stringBuilder.toString();
     }
 }

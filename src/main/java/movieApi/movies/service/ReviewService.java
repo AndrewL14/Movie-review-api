c79b8234559372cc88c7c94ef66489e72bf9db40
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-08-12 11:40:14 -0500

    added user funcionality
    added feature to also update user entity when creating a new review

--- src/main/java/movieApi/movies/service/ReviewService.java
+++ src/main/java/movieApi/movies/service/ReviewService.java
@@ -1,9 +1,10 @@
 package movieApi.movies.service;
 
 import movieApi.movies.converter.Converter;
-import movieApi.movies.dto.ReviewDTO;
+import movieApi.movies.dto.response.ReviewDTO;
 import movieApi.movies.entity.Movie;
 import movieApi.movies.entity.Review;
+import movieApi.movies.entity.User;
 import movieApi.movies.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -20,7 +21,7 @@ public class ReviewService {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    public ReviewDTO createReview(String reviewBody, String imdbId) {
+    public ReviewDTO createReview(String reviewBody, String imdbId, String userImdbId) {
         Review review = reviewRepository.insert(new Review(reviewBody));
 
         mongoTemplate.update(Movie.class)
@@ -28,6 +29,11 @@ public class ReviewService {
                 .apply(new Update().push("reviewIds").value(review))
                 .first();
 
-        return Converter.reviewDTO(review);
+        mongoTemplate.update(User.class)
+                .matching(Criteria.where("imdbId").is(userImdbId))
+                .apply(new Update().push("userReviews").value(review))
+                .first();
+
+        return Converter.reviewToDTO(review);
     }
 }

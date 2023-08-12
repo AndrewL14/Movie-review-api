e028b8aa94a1137e5a4638418ac590eac5de3831
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-08-12 11:37:34 -0500

    added user Repo

--- src/main/java/movieApi/movies/repository/UserRepository.java
+++ src/main/java/movieApi/movies/repository/UserRepository.java
@@ -1,2 +1,13 @@
-package movieApi.movies.repository;public interface UserRepository {
+package movieApi.movies.repository;
+
+import movieApi.movies.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    Optional<User> findUserByImdbId(String imdbId);
 }

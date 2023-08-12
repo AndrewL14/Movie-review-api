a2c41d5ac705e9064fd3f02c06a1933dd85553d4
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-08-12 11:37:12 -0500

    moved to  different package

--- src/main/java/movieApi/movies/dto/response/MovieDTO.java
+++ src/main/java/movieApi/movies/dto/response/MovieDTO.java
@@ -1,4 +1,4 @@
-package movieApi.movies.dto;
+package movieApi.movies.dto.response;
 
 import java.util.List;
 
--- src/main/java/movieApi/movies/dto/response/ReviewDTO.java
+++ src/main/java/movieApi/movies/dto/response/ReviewDTO.java
@@ -1,4 +1,4 @@
-package movieApi.movies.dto;
+package movieApi.movies.dto.response;
 
 public record ReviewDTO(String body) {
 }

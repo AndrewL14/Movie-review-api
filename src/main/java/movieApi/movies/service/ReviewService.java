6cfb7d20fc6f8112be539f1b5658d672c73df8e3
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-08-28 11:11:14 -0500

    exception handling
    added MovieNotFound and UserNotFound exceptions for mongotemplate query
    search. and IllegalArgs for invalid imdbIds.
     Please enter the commit message for your changes. Lines starting

--- src/main/java/movieApi/movies/service/ReviewService.java
+++ src/main/java/movieApi/movies/service/ReviewService.java
@@ -5,6 +5,8 @@ import movieApi.movies.dto.response.ReviewDTO;
 import movieApi.movies.entity.Movie;
 import movieApi.movies.entity.Review;
 import movieApi.movies.entity.User;
+import movieApi.movies.exception.MovieNotFoundException;
+import movieApi.movies.exception.UserNotFoundException;
 import movieApi.movies.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -22,17 +24,29 @@ public class ReviewService {
     private MongoTemplate mongoTemplate;
 
     public ReviewDTO createReview(String reviewBody, String imdbId, String userImdbId) {
+        if (!imdbId.startsWith("tt") || !userImdbId.startsWith("tt")) {
+            throw new IllegalArgumentException("user or movie imdbId invalid format");
+        }
+
+
         Review review = reviewRepository.insert(new Review(reviewBody));
+        try {
+            mongoTemplate.update(Movie.class)
+                    .matching(Criteria.where("imdbId").is(imdbId))
+                    .apply(new Update().push("reviewIds").value(review))
+                    .first();
+        } catch (Exception e) {
+            throw new MovieNotFoundException("Invalid Movie imdbId: " + imdbId);
+        }
 
-        mongoTemplate.update(Movie.class)
-                .matching(Criteria.where("imdbId").is(imdbId))
-                .apply(new Update().push("reviewIds").value(review))
-                .first();
-
-        mongoTemplate.update(User.class)
-                .matching(Criteria.where("imdbId").is(userImdbId))
-                .apply(new Update().push("userReviews").value(review))
-                .first();
+        try {
+            mongoTemplate.update(User.class)
+                    .matching(Criteria.where("imdbId").is(userImdbId))
+                    .apply(new Update().push("userReviews").value(review))
+                    .first();
+        } catch (Exception e) {
+            throw new UserNotFoundException("Invalid User imdbId: " + userImdbId);
+        }
 
         return Converter.reviewToDTO(review);
     }
